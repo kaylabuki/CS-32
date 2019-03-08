f75cf9bd46db00795cac3acb32ca3a8d4830e235
Kayla Buki <46634750+kaylabuki@users.noreply.github.com>
2019-03-07 21:20:43 -0800

    Add files via upload

--- Project 4/Trie.h
+++ Project 4/Trie.h
@@ -8,8 +8,21 @@ template<typename ValueType>
 class Trie
 {
 public:
-    Trie();
-    ~Trie();
+    Trie()
+	{
+		A = 0, C = 1, G = 2, N = 3, T = 4;
+		root = new TrieNode;
+		for (int i = 0; i < 5; i++)
+			root->m_ptrs[i] = nullptr;
+		root->leaf = false;
+	}
+
+	~Trie()
+	{
+		TrieNode* cur = root;
+		cur->deleteTrieNode();
+	}
+
     void reset();
     void insert(const std::string& key, const ValueType& value);
     std::vector<ValueType> find(const std::string& key, bool exactMatchOnly) const;
@@ -18,6 +31,26 @@ public:
     Trie(const Trie&) = delete;
     Trie& operator=(const Trie&) = delete;
 private:
+	struct TrieNode
+	{
+		vector<TrieNode*> m_ptrs;
+		vector<ItemType> values;
+		bool leaf;
+
+		void deleteTrieNode()
+		{
+			if (leaf)
+				delete;
+			else
+			{
+				for (int i = 0; i < 4; i++)
+					m_ptrs[i]->deleteTrieNode();
+			}
+		}
+	};
+
+	Node* root;
+	const int A, C, G, N, T;
 };
 
 #endif // TRIE_INCLUDED
--- Project 4/main.cpp
+++ Project 4/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <fstream> //needed	to	open	files
+#include <fstream> //needed	to	open files
 #include <string>
 #include <vector>
 #include "provided.h"
@@ -7,13 +7,31 @@ using namespace std;
 
 int main()
 {
-	string filename = "C:\\Users\\kayla\\source\\repos\\CS32\\Project4\\Project4\\data\\Desulfurococcus_mucosus.txt";
+	//string filename = "C:\\Users\kayla\source\repos\CS 32\Project4\Project4\data\Desulfurococcus_mucosus.txt";
+	//string filename = "Desulfurococcus_mucosus.txt";
+	string filename = "C:/Users/kayla/source/repos/CS_32/Project4/Project4/data/Desulfurococcus_mucosus.txt";
 	ifstream strm(filename);
+	if (!strm)
+		cout << "cannot open file" << endl;
+	
+	string badfile = "C:/Users/kayla/source/repos/CS_32/Project4/Project4/data/badformat.txt";
+	ifstream badstrm(badfile);
+	if (!badstrm)
+		cout << "cannot open file" << endl;
+
 	vector<Genome> vg;
-	bool success = false;
-	success = Genome::load(strm, vg);
+
+	bool success = Genome::load(strm, vg);
 	if (success)
-		cout << "Success!" << endl;
+		cout << "Success on good stream!" << endl;
 	else
-		cout << "Fail." << endl;
+		cout << "Fail on good stream." << endl;
+
+	bool bad = Genome::load(badstrm, vg);
+	if (!bad)
+		cout << "Fail on bad stream!" << endl;
+	else
+		cout << "Success on bad stream." << endl;
+
+
 }
