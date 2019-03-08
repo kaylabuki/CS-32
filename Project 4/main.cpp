7b8ba4b03dbeac64d64a6456defe7332fe87002c
Kayla Buki <46634750+kaylabuki@users.noreply.github.com>
2019-03-07 16:45:51 -0800

    Add files via upload

--- Project 4/main.cpp
+++ Project 4/main.cpp
@@ -1,4 +1,19 @@
+#include <iostream>
+#include <fstream> //needed	to	open	files
+#include <string>
+#include <vector>
+#include "provided.h"
+using namespace std;
+
 int main()
 {
-
+	string filename = "C:\\Users\\kayla\\source\\repos\\CS32\\Project4\\Project4\\data\\Desulfurococcus_mucosus.txt";
+	ifstream strm(filename);
+	vector<Genome> vg;
+	bool success = false;
+	success = Genome::load(strm, vg);
+	if (success)
+		cout << "Success!" << endl;
+	else
+		cout << "Fail." << endl;
 }
