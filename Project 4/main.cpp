0943fbb6d6654ea8a985a166e2e8f2f9decd7012
Kayla Buki <46634750+kaylabuki@users.noreply.github.com>
2019-03-09 17:15:48 -0800

    Add files via upload

--- Project 4/main.cpp
+++ Project 4/main.cpp
@@ -14,38 +14,33 @@ int main()
 	ifstream strm(filename);
 	if (!strm)
 		cout << "cannot open file" << endl;
-	
 	string badfile = "C:/Users/kayla/source/repos/CS_32/Project4/Project4/data/badformat.txt";
 	ifstream badstrm(badfile);
 	if (!badstrm)
 		cout << "cannot open file" << endl;
-
 	vector<Genome> vg;
-
 	bool success = Genome::load(strm, vg);
 	if (success)
 		cout << "Success on good stream!" << endl;
 	else
 		cout << "Fail on good stream." << endl;
-
 	bool bad = Genome::load(badstrm, vg);
 	if (!bad)
 		cout << "Fail on bad stream!" << endl;
 	else
 		cout << "Success on bad stream." << endl;
-
 	cout << "Genome.cpp = success!" << endl << endl;
 
 	*/
 
-	cout << "Testing Trie:" << endl << endl;
-
+	/*cout << "Testing Trie:" << endl << endl;
 	cout << "Creating new Trie:" << endl;
 	Trie<int> t;
 	cout << "Trie created!" << endl;
 	t.insert("abc", 3);
 	t.insert("abd", 4);
 	t.insert("acd", 5);
+	t.insert("bbc", 7);
 	cout << endl << "Test 1, should print 3:" << endl;
 	vector<int> vec1 = t.find("abc", true);
 	for (int i = 0; i < vec1.size(); i++)
@@ -77,5 +72,8 @@ int main()
 	cout << endl << "Test 8, should print nothing:" << endl;
 	vec1 = t.find("z", false);
 	for (int i = 0; i < vec1.size(); i++)
-		cout << vec1[i] << " ";
+		cout << vec1[i] << " ";*/
+
+
+
 }
